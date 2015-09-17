ddd5922a2e5283a0f980dd1b1ee6117cf1336180
Knut Hühne <knut@k-nut.eu>
2015-09-17 15:28:17 +0200

    fix indentation

--- buildcert.py
+++ buildcert.py
@@ -8,12 +8,12 @@ COMMAND = "echo"
 for request in Request.query.filter(Request.generation_date == None).all():
     print("Do you want to generate a certificate for {}, {} ?".format(request.id, request.email))
     print("Type y to continue")
-    confirm  = input('>')
+    confirm = input('>')
     if confirm in ['Y', 'y']:
-         print('generating certificate')
-         call([COMMAND, request.id, request.email])
-         request.generation_date = datetime.date.today()
-         db.session.commit()
-         print()
+        print('generating certificate')
+        call([COMMAND, request.id, request.email])
+        request.generation_date = datetime.date.today()
+        db.session.commit()
+        print()
     else:
-         print('skipping generation \n')
+        print('skipping generation \n')
