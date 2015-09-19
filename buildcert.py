721ca65e3c6d09a8d2f5f366c91688316a3bb907
Philipp Borgers <borgers@mi.fu-berlin.de>
2015-09-19 22:24:33 +0200

    buildcert.py: silence pep8 (linting)

--- buildcert.py
+++ buildcert.py
@@ -5,8 +5,9 @@ from app import Request, db
 
 COMMAND = "echo"
 
-for request in Request.query.filter(Request.generation_date == None).all():
-    print("Do you want to generate a certificate for {}, {} ?".format(request.id, request.email))
+for request in Request.query.filter(Request.generation_date == None).all():  # noqa
+    prompt = "Do you want to generate a certificate for {}, {} ?"
+    print(prompt.format(request.id, request.email))
     print("Type y to continue")
     confirm = input('>')
     if confirm in ['Y', 'y']:
