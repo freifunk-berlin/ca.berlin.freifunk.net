8c63a464f26ef5e39279dd03a837d7c5187b55a7
Knut Hühne <knut@k-nut.eu>
2015-09-29 13:46:41 +0200

    Some cleanup in import script
    - does not use reserved names for variables anymore

--- import.py
+++ import.py
@@ -2,7 +2,6 @@
 
 from app import Request, db
 from glob import glob
-from ntpath import basename
 from sqlalchemy.exc import IntegrityError
 from OpenSSL import crypto
 from datetime import datetime
@@ -11,23 +10,23 @@ DIRECTORY = "tests/openvpn/easy-rsa/keys/"
 
 
 for path in glob("{}/freifunk_*.crt".format(DIRECTORY)):
-    with open(path) as file:
+    with open(path) as certfile:
         print("Importing {} ...".format(path))
         certificate = crypto.load_certificate(
                 crypto.FILETYPE_PEM,
-                file.read()
+                certfile.read()
                 )
         # extract email and id from subject components
         components = dict(certificate.get_subject().get_components())
         email_address = components[b'emailAddress']
         # remove 'freifunk_' prefix from id
-        id = components[b'CN'].decode('utf-8').replace('freifunk_', '')
+        cert_id = components[b'CN'].decode('utf-8').replace('freifunk_', '')
         # extract creation date from certificate
         generation_date = datetime.strptime(
                 certificate.get_notBefore().decode('utf-8'),
                 '%Y%m%d%H%M%SZ'
                 )
-        request = Request(id, email_address, generation_date)
+        request = Request(cert_id, email_address, generation_date)
         try:
             db.session.add(request)
             db.session.commit()
