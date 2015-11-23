bf7975ba3b3e4acaf40df3959842f18c38341661
Knut Hühne <knut@k-nut.eu>
2015-11-23 19:23:02 +0100

    Revert "Add generation of activation url"
    This reverts commit 15d3af5070adcf6578bb4b2f11bbd709a62bdc90.

--- ca/views.py
+++ ca/views.py
@@ -1,11 +1,8 @@
-from flask import Flask, request, render_template, flash, url_for, abort
-from itsdangerous import URLSafeSerializer
+from flask import request, render_template
 
 from ca import app, db
 from ca.forms import RequestForm
 from ca.models import Request
-
-s = URLSafeSerializer(app.config['SECRET_KEY'])
 
 @app.route('/', methods=['GET'])
 def index():
@@ -18,23 +15,6 @@ def post_request():
         req = Request(form.id.data, form.email.data)
         db.session.add(req)
         db.session.commit()
-        token = s.dumps(req.id, salt='freifunk-ca-service')
-        confirm_url = url_for('get_certificate',
-                              token=token,
-                              _external=True)
-
         return render_template('thanks.html')
     else:
         return render_template('index.html', form=form)
-
-@app.route('/certificates/<token>', methods=['GET'])
-def get_certificate(token):
-    try:
-        cert_id = s.loads(token, salt='freifunk-ca-service')
-    except:
-        abort(404)
-
-    ca_req = Request.query.get_or_404(cert_id)
-    print(ca_req)
-
-    return "return key + cert here + {}".format(ca_req)
