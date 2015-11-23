63d4cd2d365c1bc8bf2243ea8bcd0327f33850c4
Philipp Borgers <borgers@mi.fu-berlin.de>
2015-11-23 20:53:20 +0100

    ca/forms.py: fix pep8 problem

--- ca/forms.py
+++ ca/forms.py
@@ -5,9 +5,11 @@ from wtforms import Form, StringField, validators, ValidationError
 from ca.models import Request
 from ca import db
 
+
 def id_does_not_exist(form, field):
     if db.session.query(Request.id).filter(Request.id == field.data).count():
         raise ValidationError('Id already exists.')
+
 
 class RequestForm(Form):
     id = StringField(
