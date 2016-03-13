211cdbee80ec9bd70ca4433ce4c16661b2a86804
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-03-13 01:57:36 +0100

    config, buildcert: implement configurable parameters
    just move all relevant paramters into config.py to have them not hardcoded

--- buildcert.py
+++ buildcert.py
@@ -18,9 +18,9 @@ def create_cert(cert_name, cert_email, cert_sn, cert_key):
     """
 
     # get required CA-data
-    ca_cert_file = open('/tmp/ffca.crt', 'r')
+    ca_cert_file = open(app.config['CACERT_FILE'], 'r')
     ca_cert = crypto.load_certificate(crypto.FILETYPE_PEM, ca_cert_file.read())
-    ca_key_file = open('/tmp/ffca.key', 'r')
+    ca_key_file = open(app.config['CAKEY_FILE'], 'r')
     ca_key = crypto.load_privatekey(crypto.FILETYPE_PEM, ca_key_file.read())
 
     if True:
@@ -28,15 +28,15 @@ def create_cert(cert_name, cert_email, cert_sn, cert_key):
         # create a self-signed cert
         cert = crypto.X509()
         cert.set_version(0x02)  # X509-Version 3
-        cert.get_subject().C = "DE"
-        cert.get_subject().ST = "Eastern Germany"
-        cert.get_subject().L = "Berlin"
-        cert.get_subject().O = "Foerderverein Freie Netzwerke e.V."
+        cert.get_subject().C = app.config['NEWCERT_COUNTRY']
+        cert.get_subject().ST = app.config['NEWCERT_STATE']
+        cert.get_subject().L = app.config['NEWCERT_LOCATION']
+        cert.get_subject().O = app.config['NEWCERT_ORGANIZATION']
         cert.get_subject().CN = "freifunk_%s" % cert_name
         cert.get_subject().emailAddress = cert_email
         cert.set_serial_number(cert_sn)
         cert.gmtime_adj_notBefore(0)
-        cert.gmtime_adj_notAfter(10*365*24*60*60)
+        cert.gmtime_adj_notAfter(app.config['NEWCERT_DURATION'])
         cert.set_issuer(ca_cert.get_subject())
         cert.set_pubkey(cert_key)
 
@@ -45,7 +45,7 @@ def create_cert(cert_name, cert_email, cert_sn, cert_key):
         #  https://github.com/pyca/pyopenssl/issues/15
         cert_ext = [
             crypto.X509Extension(b'basicConstraints', False, b"CA:FALSE"),
-            crypto.X509Extension(b'nsComment', False, b'made for you with PyOpenSSL'),
+            crypto.X509Extension(b'nsComment', False, app.config['NEWCERT_COMMENT']),
             crypto.X509Extension(b'subjectKeyIdentifier', False, b'hash', subject=cert),
             crypto.X509Extension(b'authorityKeyIdentifier', False, b'keyid:always,issuer:always', issuer=ca_cert),
             crypto.X509Extension(b'extendedKeyUsage', False, b'TLS Web Client Authentication'),
@@ -53,7 +53,7 @@ def create_cert(cert_name, cert_email, cert_sn, cert_key):
         ]
         cert.add_extensions(cert_ext)
 
-        cert.sign(ca_key, 'sha1')
+        cert.sign(ca_key, app.config['NEWCERT_SIGNDIGEST'])
 
         print (crypto.dump_certificate(crypto.FILETYPE_TEXT, cert))
         return (cert)
@@ -62,7 +62,11 @@ def create_key():
         """Create a 1024 RSA key-pair"""
         # create a key pair
         k = crypto.PKey()
-        k.generate_key(crypto.TYPE_RSA, 1024)
+        if app.config['NEWKEY_ALG'].lower() == 'rsa':
+            keytype = crypto.TYPE_RSA
+        elif app.config['NEWKEY_ALG'].lower() == 'dsa':
+            keytype = crypto.TYPE_DSA
+        k.generate_key(keytype, app.config['NEWKEY_SIZE'])
         # crypto.dump_privatekey(crypto.FILETYPE_PEM, k)
         return (k)
 
