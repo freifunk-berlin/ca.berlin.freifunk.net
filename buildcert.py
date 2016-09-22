42496167d8cb8658818514a2bc6aa8c9f96d5a8b
KenTacki <out2222@posteo.de>
2016-09-22 15:59:41 +0200

    Make Subject configurable.

--- buildcert.py
+++ buildcert.py
@@ -13,7 +13,7 @@ from flask_mail import Message
 
 def mail_certificate(id, email):
     with app.app_context():
-        msg = Message('Freifunk Vpn03 Key', sender = app.config['MAIL_FROM'], recipients = [email])
+        msg = Message(app.config['MAIL_SUBJECT'], sender = app.config['MAIL_FROM'], recipients = [email])
         msg.body = render_template('mail.txt')
         with app.open_resource("{}/freifunk_{}.tgz".format(app.config['DIRECTORY_CLIENTS'], id)) as fp:
             msg.attach("freifunk_{}.tgz".format(id), "application/gzip", fp.read())
