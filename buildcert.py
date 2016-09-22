494b628ab57c38335368a1b7a2734c7abafc5277
KenTacki <out2222@posteo.de>
2016-09-22 14:23:46 +0200

    Add mail_certificate which sends email with flask-mail
    Replace COMMAND_MAIL. Send certs from /etc/openvpn/client/
    Use template /templates/mail.txt

--- buildcert.py
+++ buildcert.py
@@ -7,6 +7,17 @@ from subprocess import call
 from ca import app, db
 from ca.models import Request
 
+from flask import Flask, render_template
+from flask_mail import Mail, Message
+
+
+def mail_certificate(id, email):
+    msg = Message('Freifunk Vpn03 Key', sender = 'no-reply@ca.berlin.freifunk.net', recipients = [email])
+    msg.body = render_template('mail.txt')
+    with app.open_resource("/etc/openvpn/client/freifunk_{}.tgz".format(id)) as fp:
+        msg.attach("freifunk_{}.tgz".format(id), "application/gzip", fp.read())
+    mail.send(msg)
+
 for request in Request.query.filter(Request.generation_date == None).all():  # noqa
     prompt = "Do you want to generate a certificate for {}, {} ?"
     print(prompt.format(request.id, request.email))
@@ -15,7 +26,8 @@ for request in Request.query.filter(Request.generation_date == None).all():  # n
     if confirm in ['Y', 'y']:
         print('generating certificate')
         call([app.config['COMMAND_BUILD'], request.id, request.email])
-        call([app.config['COMMAND_MAIL'], request.id, request.email])
+        #call([app.config['COMMAND_MAIL'], request.id, request.email])
+        mail_certificate(request.id, request.email)
         request.generation_date = datetime.date.today()
         db.session.commit()
         print()
