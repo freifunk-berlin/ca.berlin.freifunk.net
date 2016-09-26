b8a971424b5d1352186b77cb4b7ae1a8e28fa83c
KenTacki <out2222@posteo.de>
2016-09-26 21:01:44 +0200

    Remove dead COMMAND_MAIL in config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 SECRET_KEY = 'foobar'
 DIRECTORY = "tests/openvpn/easy-rsa/keys/"
 COMMAND_BUILD = "tests/openvpn/clients/openvpn-build-key"
-COMMAND_MAIL = "tests/openvpn/clients/openvpn-mail-key"
 MAIL_FROM = "no-reply@ca.berlin.freifunk.net"
 MAIL_SUBJECT = "Freifunk VPN03 Zugangsdaten"
 DIRECTORY_CLIENTS = "/etc/openvpn/clients/"
