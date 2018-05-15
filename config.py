5407d2d030bf66f96ebf63c8a385007532408175
sarumpaet <sarumpaet@users.noreply.github.com>
2018-05-15 08:07:11 +0200

    add tunnel.berlin.freifunk.net settings

--- config.py
+++ config.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
+# override these in instance/config.py
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 SECRET_KEY = 'foobar'
 DENY_EXEC_AS_ROOT = True
 DIRECTORY = "tests/openvpn/easy-rsa/keys/"
-MAIL_FROM = "no-reply@ca.berlin.freifunk.net"
-MAIL_SUBJECT = "Freifunk VPN03 Zugangsdaten"
-SHOW_SIGNED_REQUESTS = False
-
 CACERT_FILE = '/tmp/ffca.crt'
 CAKEY_FILE = '/tmp/ffca.key'
+
+# tunnel.berlin.freifunk.net settings
+SHOW_SIGNED_REQUESTS = True
+MAIL_FROM = "no-reply@tunnel.berlin.freifunk.net"
+MAIL_SUBJECT = "Freifunk Tunnel-Berlin Zugangsdaten"
 NEWKEY_ALG = 'rsa'
-NEWKEY_SIZE = 1024
+NEWKEY_SIZE = 2048
 NEWCERT_COUNTRY = "DE"
 NEWCERT_STATE = "Eastern Germany"
 NEWCERT_LOCATION = "Berlin"
-NEWCERT_ORGANIZATION = "Foerderverein Freie Netzwerke e.V."
+NEWCERT_ORGANIZATION = "Freifunk Community Berlin"
 NEWCERT_DURATION = 10*365*24*60*60 # 10 years
-NEWCERT_COMMENT = b'made for you with PyOpenSSL'
-NEWCERT_SIGNDIGEST = "sha1"
+NEWCERT_COMMENT = b'initial test-certs'
+NEWCERT_SIGNDIGEST = "sha256"
